d9c22d276e7f47cbaf7a4e2f9badc686e1623dd9
maliut <lqn619@163.com>
2025-02-26 00:55:12 +0800

    fix ts

--- packages/demo/src/vite-env.d.ts
+++ packages/demo/src/vite-env.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="vite/client" />
 
+import type { Container } from 'pixi.js'
 import type { PixiReactElementProps } from '@pixi/react'
 import type { FlexContainer } from '@florastudio/pixi-flex'
 
 declare module '@pixi/react' {
   interface PixiElements {
-    flexContainer: PixiReactElementProps<typeof FlexContainer>;
+    flexContainer: PixiReactElementProps<typeof FlexContainer<Container>>;
   }
 }
